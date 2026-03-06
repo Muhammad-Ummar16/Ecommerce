8a23a471b1f1677867bcb0313548e89101690180
muhammad-ummar16 <l1f22bsse0016@ucp.edu.pk>
2026-03-06 11:40:47 +0500

    resove some errors

--- backend/db/connect.js
+++ backend/db/connect.js
@@ -1,19 +1,37 @@
 import mongoose from "mongoose";
 
+let cached = global.mongoose;
+
+if (!cached) {
+    cached = global.mongoose = { conn: null, promise: null };
+}
+
 const connectDB = async () => {
-    try {
-        const conn = await mongoose.connect(process.env.MONGODB_URI, {
+    if (cached.conn) {
+        return cached.conn;
+    }
+
+    if (!cached.promise) {
+        cached.promise = mongoose.connect(process.env.MONGODB_URI, {
             serverApi: {
                 version: "1",
                 strict: true,
                 deprecationErrors: true,
-            }
-        });
-        console.log(`MongoDB Connected: ${conn.connection.host}`);
+            },
+            bufferCommands: false,
+        }).then((mongoose) => mongoose);
+    }
+
+    try {
+        cached.conn = await cached.promise;
+        console.log(`MongoDB Connected: ${cached.conn.connection.host}`);
     } catch (error) {
+        cached.promise = null;
         console.error(`Error: ${error.message}`);
         process.exit(1);
     }
+
+    return cached.conn;
 };
 
 export default connectDB;
